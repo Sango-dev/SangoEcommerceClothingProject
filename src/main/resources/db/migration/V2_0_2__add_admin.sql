e52e200fadfea09b9a8e9299502f26e5d942f8fa
Sango-dev <klimencuks@gmail.com>
2023-06-08 13:53:56 +0300

    change admin data

--- src/main/resources/db/migration/V2_0_2__add_admin.sql
+++ src/main/resources/db/migration/V2_0_2__add_admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO users(id, nick_name, first_name, last_name, password, email, phone, role)
-VALUES ('9782e1d4-4483-11ed-b878-0242ac120002', 'admin', 'Oleksandr', 'Klymenchuk', '$2a$10$DMUxmTkIjN1bkhJZQuuWm.yz1OzYKbHf163Tra4SLcIJjgeqJQ8Si', 'adm@gmail.com', '0677176725', 'ADMIN');
+VALUES ('9782e1d4-4483-11ed-b878-0242ac120002', 'admin', 'Олександр', 'Клименчук', '$2a$10$DMUxmTkIjN1bkhJZQuuWm.yz1OzYKbHf163Tra4SLcIJjgeqJQ8Si', 'adm@gmail.com', '0677176725', 'ADMIN');
